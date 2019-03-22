e64fa341e5fbbb4b0ee6ac107101c85d53234e51
wxp9803 <wxp9803@rit.edu>
2019-03-22 10:51:45 -0400

    Added Scene with enemies

---
+++ Dynamic Defense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = Time.deltaTime * speed;
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, new Vector3 (0.0f,.5f,0.0f), step);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag=="Player")
+        {
+            gameObject.SetActive(false);
+        }
+            
+    }
+    public void setSpeed(float _speed)
+    {
+        speed = _speed;
+    }
+    public float getSpeed()
+    {
+        return speed;
+    }
+
+}
---
+++ Dynamic Defense/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject enemy1;
+    private List<GameObject> enemyList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyList = new List<GameObject>();
+        for(int i = 0;i<10;i++)
+        {
+            GameObject g = Instantiate(enemy1, randPos(),Quaternion.identity);
+            g.GetComponent<Enemy>().setSpeed(2.0f);
+            enemyList.Add(g);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    Vector3 randPos()
+    {
+        return new Vector3(UnityEngine.Random.Range(-35.0f, 35.0f), .5f, UnityEngine.Random.Range(-35.0f, 35.0f));
+    }
+}
