1e3d0cc67049667616d7d06a8c2b1b8f005b6037
stevensalex29 <stevensalex29@gmail.com>
2019-03-25 10:57:35 -0400

    gameover on hit by enemy

---
+++ Dynamic Defense/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver");
+        }
+    }
+}
