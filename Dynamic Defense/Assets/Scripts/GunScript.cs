3ff2978cc1a6fac793c07266fa11e92b99d18a58
Tung Nguyen (RIT Student) <thn4185@ad.rit.edu>
2019-03-24 22:29:16 -0400

    Added FPS controller with shooting.

---
+++ Dynamic Defense/Assets/Scripts/GunScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newBullet = Instantiate(bullet, startPos.position, transform.rotation);
+            newBullet.GetComponent<Rigidbody>().AddForce(transform.forward * 4000.0f);
+        }
+    }
+}
